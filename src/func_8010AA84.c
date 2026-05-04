f3f480ced7050895cbbc56a7e8eff91ceb148b9a
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-04 23:55:23 +0000

    match: func_8010AA84 (sibling of AAD8, passes arg4 not arg4[8] to func_80008650)

---
+++ src/func_8010AA84.c
@@ -0,0 +1,14 @@
+#include "common.h"
+
+extern void *D_8004AC44;
+
+extern void func_8000A9D8(void *, s32, s32, s32, void *);
+extern void func_80008650(void *, void *);
+
+void func_8010AA84(void *arg0, s32 arg1, s32 arg2, void *arg3, void *arg4) {
+    if (arg0 == 0) {
+        arg0 = D_8004AC44;
+    }
+    func_8000A9D8(arg0, arg1, *(s32 *) ((u8 *) arg4 + 0x28), arg2, arg3);
+    func_80008650(arg0, arg4);
+}
