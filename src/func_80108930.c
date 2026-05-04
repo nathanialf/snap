38fe7c86829cb7f502e575a40d34fcf5dc659462
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-04 23:04:13 +0000

    consolidate: merge func_80108960 (4-instr free-list push) into func_80108930.c

--- src/func_80108930.c
+++ src/func_80108930.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+extern void *D_8004AC10;
+extern s32 D_8004AC14;
+
 void func_80108930(u8 *arg0, u8 *arg1) {
     *(u8 **) arg1 = *(u8 **) (arg0 + 0x6C);
     *(u8 **) (arg0 + 0x6C) = arg1;
@@ -13,4 +16,10 @@ void func_80108940(u8 *arg0, u8 *arg1) {
 void func_80108950(u8 *arg0, u8 *arg1) {
     *(u8 **) arg1 = *(u8 **) (arg0 + 0x6C);
     *(u8 **) (arg0 + 0x6C) = arg1;
+}
+
+void func_80108960(void **arg0) {
+    arg0[0] = D_8004AC10;
+    D_8004AC10 = arg0;
+    D_8004AC14--;
 }
