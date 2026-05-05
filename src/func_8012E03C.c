df51fd179a0269b8e98d8540de8f353d7576f17b
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-05 04:12:29 +0000

    consolidate: merge 2EC3C..2ED27 (3 dispatch + msg-send funcs) into func_8012E03C.c

--- src/func_8012E03C.c
+++ src/func_8012E03C.c
@@ -1,5 +1,17 @@
 #include "common.h"
 
+extern s32 func_80029E80(void *queue, void *msg, s32 flag);
+
+typedef struct {
+    s16 type;
+    s16 _pad0;
+    s32 zero;
+    s8  byte_a;
+    s8  byte_b;
+    s8  byte_c;
+    s8  _pad[5];
+} ScreenMsg;
+
 s32 func_8012E03C(s32 *arg0, s32 arg1, s32 arg2) {
     switch (arg1) {
         case 1:
@@ -10,4 +22,32 @@ s32 func_8012E03C(s32 *arg0, s32 arg1, s32 arg2) {
             break;
     }
     return 0;
+}
+
+void func_8012E070(thr, arg1, arg2)
+s32 *thr;
+char arg1;
+int arg2;
+{
+    ScreenMsg msg;
+    msg.type = 2;
+    msg.zero = 0;
+    msg.byte_a = arg1 | 0xB0;
+    msg.byte_b = 0x07;
+    msg.byte_c = ((unsigned char *) &arg2)[3];
+    func_80029E80((u8 *)thr + 0x4C, &msg, 0);
+}
+
+void func_8012E0CC(thr, arg1, arg2)
+s32 *thr;
+char arg1;
+int arg2;
+{
+    ScreenMsg msg;
+    msg.type = 2;
+    msg.zero = 0;
+    msg.byte_a = arg1 | 0xB0;
+    msg.byte_b = 0x08;
+    msg.byte_c = ((char *) &arg2)[3];
+    func_80029E80((u8 *)thr + 0x4C, &msg, 0);
 }
