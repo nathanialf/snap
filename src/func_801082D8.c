9c67e8bcabe5b5c77eb436103b95dbb6be749aaa
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-05 01:42:36 +0000

    consolidate: merge 8ED8..8F94 (4 controller-state getters + setter) into func_801082D8.c
    Resolved D_8004AC50 type conflict (s32 vs s32 *) by declaring s32 *
    and casting in func_801082D8's return.

--- src/func_801082D8.c
+++ src/func_801082D8.c
@@ -1,7 +1,36 @@
 #include "common.h"
 
-extern s32 D_8004AC50;
+extern s32 *D_8004AC50;
+extern s32 D_8004A9A8;
 
 s32 func_801082D8(void) {
-    return D_8004AC50;
+    return (s32) D_8004AC50;
+}
+
+s32 func_801082E4(s32 *arg0) {
+    s32 r = 0;
+    if (arg0 == 0) {
+        arg0 = D_8004AC50;
+    }
+    if (arg0 != 0 && *(u8 *) ((u8 *) arg0 + 0x14) == 0) {
+        r = *(s32 *) ((u8 *) (*(s32 **) ((u8 *) arg0 + 0x1C)) + 0x1B8);
+        return r;
+    }
+    return r;
+}
+
+s32 func_8010831C(s32 *arg0) {
+    s32 r = 0;
+    if (arg0 == 0) {
+        arg0 = D_8004AC50;
+    }
+    if (arg0 != 0 && *(u8 *) ((u8 *) arg0 + 0x14) == 0) {
+        r = *(s32 *) ((u8 *) (*(s32 **) ((u8 *) arg0 + 0x1C)) + 0x1BC);
+        return r;
+    }
+    return r;
+}
+
+void func_80108354(s32 arg0) {
+    D_8004A9A8 = arg0;
 }
