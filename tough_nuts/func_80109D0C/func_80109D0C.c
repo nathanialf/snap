23412bf275ddfc7377fb458eaee0eee436d77b41
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-05 19:54:29 +0000

    tough_nuts/func_80109D0C: update seed with arr[i]+1/+2/-1 attempt

--- tough_nuts/func_80109D0C/func_80109D0C.c
+++ tough_nuts/func_80109D0C/func_80109D0C.c
@@ -9,10 +9,7 @@ typedef struct Foo {
     u8 _56;
     u8 _57;
     s32 _58;
-    s32 _5C;
-    s32 _60;
-    s32 _64;
-    s32 _68;
+    s32 arr[4];
     s32 _6C;
     s32 _70;
     f32 _74;
@@ -25,15 +22,16 @@ typedef struct Foo {
 extern f32 D_800436F8;
 
 s32 func_80109D0C(Foo *arg0) {
+    register s32 i = 1;
     arg0->_4C = 0;
     arg0->_54 = 0;
     arg0->_55 = 0;
     arg0->_56 = 0;
     arg0->_58 = 0;
-    arg0->_5C = 0;
-    arg0->_60 = 0;
-    arg0->_64 = 0;
-    arg0->_68 = 0;
+    arg0->arr[i] = 0;
+    arg0->arr[i + 1] = 0;
+    arg0->arr[i + 2] = 0;
+    arg0->arr[i - 1] = 0;
     arg0->_6C = 0;
     arg0->_70 = 0;
     arg0->_74 = D_800436F8;
@@ -41,5 +39,5 @@ s32 func_80109D0C(Foo *arg0) {
     arg0->_7C = 0.0f;
     arg0->_80 = 0;
     arg0->_84 = 0;
-    return 1;
+    return i;
 }
