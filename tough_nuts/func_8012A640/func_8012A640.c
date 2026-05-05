56f1bee13f836f636ce38655a62fddccca6b207c
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-05 21:51:10 +0000

    tough_nuts/func_8012A640: improved seed (msg-decl-first + filler-after gives target frame layout, scheduling drifts on ~10 insns)

--- tough_nuts/func_8012A640/func_8012A640.c
+++ tough_nuts/func_8012A640/func_8012A640.c
@@ -3,10 +3,8 @@
 extern s32 func_80029E80(void *queue, void *msg, s32 flag);
 
 typedef struct {
-    s32 _pad0;
-    s32 _pad1;
     s16 type;
-    s16 _pad;
+    s16 pad;
     s32 ptr;
     f32 fval;
     s32 _unused;
@@ -14,8 +12,9 @@ typedef struct {
 
 void func_8012A640(s32 *arg0, f32 arg1) {
     Msg16 msg;
-    msg.ptr = (s32) ((u8 *) arg0[16] + arg0[15] * 48);
+    s32 _filler[2];
     msg.type = 4;
+    msg.ptr = (s32) ((u8 *) arg0[16] + arg0[15] * 48);
     msg.fval = arg1;
     func_80029E80((u8 *) arg0 + 0x14, &msg, 0);
 }
