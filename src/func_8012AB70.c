1e34d2f59d5b7493956bad60dd5c8fbcf7e46894
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-05 01:30:19 +0000

    consolidate: merge 2B770..2B850 (4 list-pop/push/drain/erase) into func_8012AB70.c
    Resolved the func_80029CA0/func_80029C7C type-conflict by declaring
    them `void *` and casting at the s32 call sites.

--- src/func_8012AB70.c
+++ src/func_8012AB70.c
@@ -2,6 +2,9 @@
 
 extern s32 *D_800428F0;
 
+extern void func_80029CA0(void *);
+extern void func_80029C7C(void *, s32 *);
+
 s32 *func_8012AB70(void) {
     s32 *r = 0;
     s32 *p = D_800428F0;
@@ -12,4 +15,26 @@ s32 *func_8012AB70(void) {
         head[0] = 0;
     }
     return r;
+}
+
+void func_8012ABA0(s32 *arg0) {
+    s32 *p = D_800428F0;
+    arg0[0] = p[0xB];
+    p[0xB] = (s32) arg0;
+}
+
+void func_8012ABB8(s32 *arg0) {
+    s32 *node = (s32 *) arg0[0x14 / 4];
+    if (node != 0) {
+        do {
+            func_80029CA0(node);
+            func_80029C7C(node, &arg0[0x4 / 4]);
+            node = (s32 *) arg0[0x14 / 4];
+        } while (node != 0);
+    }
+}
+
+void func_8012AC18(s32 *arg0, s32 arg1) {
+    func_80029CA0((void *) arg1);
+    func_80029C7C((void *) arg1, arg0 + 5);
 }
--- src/func_8012AC18.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-extern void func_80029CA0(s32);
-extern s32 func_80029C7C(s32, s32 *);
-
-void func_8012AC18(s32 *arg0, s32 arg1) {
-    func_80029CA0(arg1);
-    func_80029C7C(arg1, arg0 + 5);
-}
