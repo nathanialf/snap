471859018ffb53ee459387f7d57c170e42716a5d
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 03:58:33 +0000

    consolidate: merge 7A24..7B8C (4 funcs) into func_80106E24.c
    Game-frame dispatch cluster: dispatcher pair (D_8004A948 hook + post-cb)
    and frame-runner pair (controller-event drain + present-and-flip).
    Reconciled D_8004A948 signatures to permissive `void (*)()`.

--- src/func_80106E24.c
+++ src/func_80106E24.c
@@ -1,8 +1,51 @@
 #include "common.h"
 
 extern void (*D_8004A948)();
+extern s32 D_8004A910;
+extern s32 D_8004A848[];
+
+extern void func_800054D0(void);
+extern void func_800055D0(void);
+extern void func_80005F34(void);
+extern void func_80005A1C(void);
+extern void func_80007AFC(s32);
+extern s32 func_80006804(void);
+extern void func_8000C274(void);
 
 void func_80106E24(s32 arg0) {
     D_8004A948(arg0);
     (*(void (**)()) ((u8 *) arg0 + 0x4))();
+}
+
+void func_80106E5C(arg0)
+s32 *arg0;
+{
+    func_800054D0();
+    func_800055D0();
+    ((void (*)(void)) arg0[3])();
+    func_80005F34();
+    func_80007AFC(D_8004A848[D_8004A910]);
+    func_80005A1C();
+}
+
+void func_80106EC0(s32 *arg0) {
+    D_8004A948(arg0);
+    ((void (*)(void)) arg0[1])();
+    if (func_80006804() != 0) {
+        func_8000C274();
+    }
+}
+
+void func_80106F10(arg0)
+s32 *arg0;
+{
+    func_800054D0();
+    func_800055D0();
+    ((void (*)(void)) arg0[3])();
+    func_80005F34();
+    func_80007AFC(D_8004A848[D_8004A910]);
+    func_80005A1C();
+    if (func_80006804() != 0) {
+        func_8000C274();
+    }
 }
